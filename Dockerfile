dcdd1c6ef6f28098084c6fb3276736cdb078e32c
Yongho Kim <ironyonghokim@gmail.com>
2022-04-27 14:22:04 -0500

    Moved backend storage to Redis

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM waggle/plugin-base:1.1.1-base
 
 COPY . /app/
-RUN pip3 install -r requirements.txt
+RUN pip3 install -r /app/requirements.txt
 
 ENTRYPOINT ["python3", "/app/checker.py"]
--- checker.py
+++ checker.py
@@ -1,34 +1,16 @@
+from os import getenv
+import re
+
+import numpy as np
+
+import redis
 from flask import Flask, request
 
 app = Flask(__name__)
 
-in_memory_db = {}
-
-@app.route("/store", methods=["POST"])
-def store():
-    global in_memory_db
-    if request.content_type == None or not request.content_type.startswith("application/json"):
-        return {
-            "error": "content_type must be application/json"
-        }
-    j = request.json
-    k =j.get("key", "")
-    v = j.get("value", "")
-    if all([k, v]):
-        in_memory_db[k] = v
-        return {
-            "response": "success",
-            "result": f'key {k} with value {v} is added'
-        }
-    else:
-        return {
-            "error": "must have key and value"
-        }
-
 
 @app.route("/evaluate", methods=["POST"])
 def evaluate():
-    global in_memory_db
     if request.content_type == None or not request.content_type.startswith("application/json"):
         return {
             "error": "content_type must be application/json"
@@ -39,11 +21,36 @@ def evaluate():
         return {
             "error": "no rule is given"
         }
-    return {
-        "response": "success",
-        "rule": rule,
-        "result": eval(rule, in_memory_db)
-    }
+    with redis.Redis(host=getenv("REDIS_HOST", "localhost"), port=getenv("REDIS_PORT", 6379), decode_responses=True) as r:
+        def avg(vs):
+            return np.mean(vs)
+        # !!! main function of interest compact notation for selecting measurements
+        def v(name, **meta):
+            value = r.get(name)
+            if value == None:
+                raise Exception(f'{name} does not exist')
+            if re.match(r"[-+]?\d*\.\d+|[-+]?\d+", value):
+                return float(value)
+            else:
+                return value
+            # return np.array([m["value"] for m in measurements if m["name"] == name and matchmeta(meta, m["meta"])])
+
+        def matchmeta(pattern, meta):
+            return all(k in meta and meta[k] == v for k, v in pattern.items())
+
+        try:
+            return {
+                "response": "success",
+                "rule": rule,
+                "result": eval(rule, None, locals())
+            }
+        except Exception as ex:
+            return {
+                "response": "failed",
+                "rule": rule,
+                "error": str(ex)
+            }
+        
 
 if __name__ == "__main__":
     app.run()
